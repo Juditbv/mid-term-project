37e9808af9ef628977c6723d3b9b7a6c616f9718
Judit Bancells Vélez <judit@juditbancells.cat>
2022-09-22 17:47:13 +0200

    refactor

--- js/functions.js
+++ js/functions.js
@@ -62,10 +62,6 @@ function addContentPost(postId) {
           src="${getImageUrl(content.id)}"
           alt="Featured image project"
         />
-        <img
-          src="${getImageUrl(content.id)}"
-          alt="Blurred image"
-        />
       </div>
         <p class="margin-small-top">
           ${content.body}
